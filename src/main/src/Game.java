054c22bf219a89dfa9d403d64b41aaf69e36b28e
Jolx77 <joaquin.otalora@mi.unc.edu.ar>
2023-04-29 19:32:31 -0300

    Inicio de clases

---
+++ src/main/src/Game.java
@@ -0,0 +1,9 @@
+public class Game {
+
+    private  gameScreen gameScreen;
+
+    public Game(){
+        gameScreen = new gameScreen();
+    }
+
+}
---
+++ src/main/src/gameScreen.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class gameScreen {
+    private JFrame jframe;
+
+    public gameScreen() {
+        jframe = new JFrame();
+        jframe.setSize(400, 400);
+    }
+}
