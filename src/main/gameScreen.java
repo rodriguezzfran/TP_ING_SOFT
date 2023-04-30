dffe35849ed099faf4e1eeb468236886949d2ad4
Franco Rodríguez <francorod47@gmail.com>
2023-04-29 21:39:41 -0300

    Update gameScreen.java

--- src/main/gameScreen.java
+++ src/main/gameScreen.java
@@ -15,6 +15,8 @@ import java.awt.event.KeyListener;
 public class gameScreen extends JPanel{
     private int deltaX=0;
     private int deltaY=0;
+    private int frames = 0;
+    private long contFPS = 0;
     public gameScreen(){
         addKeyListener(new keyboardImputs(this));
         addMouseListener(new mouseImputs());
@@ -23,15 +25,22 @@ public class gameScreen extends JPanel{
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         g.fillRect(100+deltaX,100+deltaY,200,50);
+        repaint();
+        frames++;
+        if((System.currentTimeMillis() - contFPS) >= 1000 ) {
+            contFPS = System.currentTimeMillis();
+            System.out.println("FPS: " + frames);
+            frames = 0;
+        }
     }
 
     public void cambiarDeltaX(int value){
         this.deltaX += value;
-        repaint();
+        //repaint();
     }
     public void cambiarDeltaY(int value){
         this.deltaY += value;
-        repaint();
+        //repaint();
     }
 
 }
