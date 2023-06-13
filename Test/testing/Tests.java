0b0e1c891ac42f0c1e16714e36766256bd9b592e
federicorichter <federicorichter123@gmail.com>
2023-06-13 12:49:06 -0300

    Sumamos paquete Test

--- Test/testing/Tests.java
+++ Test/testing/Tests.java
@@ -1,13 +1,11 @@
-package Testing;
+package testing;
 
+import gamestates.Playing;
 import junit.framework.TestCase;
 import main.Game;
 
-<<<<<<<< Updated upstream:src/Testing/Tests.java
-========
 import java.awt.*;
 import java.awt.event.KeyEvent;
->>>>>>>> Stashed changes:Test/testing/Tests.java
 import java.awt.geom.Rectangle2D;
 
 import static utilz.HelpMethods.*;
@@ -17,8 +15,15 @@ public class Tests extends TestCase {
     int xTest=259;
     int yTest=435;
     Rectangle2D.Float hitBox;
+    Game game;
+    Playing playing;
     public void jibos(){
         hitBox = new Rectangle2D.Float(xTest,yTest,20* Game.SCALE, 27*Game.SCALE);
+    }
+
+    public void createNewGame(){
+        game  = new Game();
+        playing = new Playing(game);
     }
     public void testGetEntityXPosNextToWallRight(){
         jibos();
@@ -39,15 +44,13 @@ public class Tests extends TestCase {
         jibos();
         assertEquals(384,(int) GetEntityYPosUnderRoofOrAboveFloor(hitBox,-2.25f)); //87 es lo que debería volver
     }
-<<<<<<<< Updated upstream:src/Testing/Tests.java
-========
 
     public void testMoveRight(){
         createNewGame();
         Button a = new Button();
         KeyEvent e = new KeyEvent(a, 401, 1686669135712l, 0, 68, 'd');
+        assertEquals(true,playing.getPlayer().getRight());
 
     }
 
->>>>>>>> Stashed changes:Test/testing/Tests.java
 }
