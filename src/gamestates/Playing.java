76e97e3516950cd046078e2649d678ab39254ee5
federicorichter <federicorichter123@gmail.com>
2023-06-16 19:36:29 -0300

    Tests nuevos

--- src/gamestates/Playing.java
+++ src/gamestates/Playing.java
@@ -175,10 +175,10 @@ public class Playing extends State implements StateMethods {
                     player.setDown(true);
                     break;
                 case KeyEvent.VK_D:
-                    System.out.println(e.getComponent() + "  " + e.getID() + "  " + e.getWhen() + "  " + e.getModifiers() + " " + e.getKeyCode() + "  "+ e.getKeyChar());
                     player.setRight(true);
                     break;
                 case KeyEvent.VK_SPACE:
+                    System.out.println(e.getComponent() + "  " + e.getID() + "  " + e.getWhen() + "  " + e.getModifiers() + " " + e.getKeyCode() + "  "+ e.getKeyChar());
                     player.setJump(true);
                     break;
                 case KeyEvent.VK_ESCAPE:
--- src/imputs/MouseInputs.java
+++ src/imputs/MouseInputs.java
@@ -1,4 +1,4 @@
-package Imputs;
+package imputs;
 
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
